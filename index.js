07b069b54fa6589875ba44eb42f3f8bd0b2b6cd2
github username <github email address>
2018-07-24 12:21:32 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function countdown(other) {
+  setTimeout(() => { other()}, 1000)
+}
+
+function createMultiplier(multiplyValue) {
+  return (value) => { return multiplyValue * value}
+}
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
